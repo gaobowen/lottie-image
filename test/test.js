3514b49b241bb866ea6905c6853b689dd6218d37
gaobowen <gaobowen@shouhuichina.cn>
2022-04-21 09:57:27 +0800

    fix bug

--- test/test.js
+++ test/test.js
@@ -17,19 +17,20 @@ var expect = require('chai').expect;
 
 
 ; (async () => {
-    // var lticvter = new LottieConverter({
-    //     chromePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    //     ffmpegPath: '/Users/gaobowen/Downloads/test2/ffmpeg'
-    // });
-    var svgcvter = new SvgConverter({
-        chromePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    var lticvter = new LottieConverter({
+        chromePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+        ffmpegPath: "D:\\chromedownload\\ffmpeg-4.3.1-win32-static-lgpl\\bin\\ffmpeg.exe"
     });
+    // var svgcvter = new SvgConverter({
+    //     chromePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    // });
 
-    // var returl = await lticvter.saveAs({
-    //     lottiePath:'/Users/gaobowen/Downloads/test2/lottie2.json',
-    //     outputPath:'/Users/gaobowen/Downloads/test2/lottie2.gif'
-    // })
-    // console.log(returl);
+    var returl = await lticvter.saveAs({
+        lottiePath:'D:\\github\\lottie-image\\test\\lottie2.json',
+        outputPath:'D:/github/lottie-image/test/test.apng'
+    })
+    console.log(returl);
+
     // await svgcvter.svgSaveAs({
     //     filePath:'/Users/gaobowen/Downloads/222.svg',
     //     outputPath:'/Users/gaobowen/Downloads/222.png',
@@ -37,21 +38,21 @@ var expect = require('chai').expect;
     //     height:'1080'
     // })
 
-    await svgcvter.svgSaveAs({
-        filePath: __dirname + '/test_clip.svg',
-        outputPath: __dirname + '/test_clip.png',
-        width: '254.6',
-        height: '254.6',
-        clipData: {
-            clipid: 'lhImageFrame',
-            imgPath:  __dirname + '/test.jpg',
-            imgw: 200.02066773076928,
-            imgh: 132.01364070230773,
-            imgx: 46.799999237060575,
-            imgy: 66.7150068664551,
-            imgr: 29.200863558049512
-        }
-    })
+    // await svgcvter.svgSaveAs({
+    //     filePath: __dirname + '/test_clip.svg',
+    //     outputPath: __dirname + '/test_clip.png',
+    //     width: '254.6',
+    //     height: '254.6',
+    //     clipData: {
+    //         clipid: 'lhImageFrame',
+    //         imgPath:  __dirname + '/test.jpg',
+    //         imgw: 200.02066773076928,
+    //         imgh: 132.01364070230773,
+    //         imgx: 46.799999237060575,
+    //         imgy: 66.7150068664551,
+    //         imgr: 29.200863558049512
+    //     }
+    // })
     // return Promise.resolve();
 })()
 
